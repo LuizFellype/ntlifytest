939002c78113648f8413e5403000d47a77fedfea
Luiz Fellype <luiz.cassago@xdevel.com.br>
2018-09-28 20:59:53 -0300

    feat: this is it

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>
+            MAE, Quero ser Programador! Vamos parar de gastar dinheio com facul de Psiciologia. kk
+          </h2>
+          <h2>
+            C##
+            eu passar pra ufes ? fico no curso : foco no que realmente quero trabar;  
+          </h2>
+
+        </header>
+        <p className="App-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
